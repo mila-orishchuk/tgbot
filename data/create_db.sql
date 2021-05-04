066f1181300afd7b5be3d40b8f4c01c09edd44fb
mila-orishchuk <mi.otishchuk@gmail.com>
2021-05-04 17:49:55 +0300

    change db structure

--- data/create_db.sql
+++ data/create_db.sql
@@ -1,44 +1,53 @@
-CREATE DATABASE recipes;
-USE recipes;
+-- This SQL script creates an empty database to store information about recipes, ingredients, measurements.
 
 CREATE TABLE IF NOT EXISTS recipes (
-    id                  bigserial PRIMARY KEY,
-    recipes_name        varchar(256) NOT NULL,
-    cooking_time        int NOT NULL,
-    description         text,
-    portion             varchar(128) NOT NULL
+    id                  INTEGER PRIMARY KEY,
+    recipes_name        VARCHAR(256) NOT NULL,
+    cooking_time        INTEGER NOT NULL,
+    description         TEXT,
+    url                 VARCHAR(256) NOT NULL,
+    img                 VARCHAR(256) NOT NULL,
+    portion             VARCHAR(128) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS ingredients (
-    id                  bigserial PRIMARY KEY,
-    name                varchar(128) NOT NULL,
-    measurements_id     bigint REFERENCES measurements(id) ON DELETE CASCADE,
-    PRIMARY KEY (measurements_id)
-
+    id                  INTEGER PRIMARY KEY,
+    name                VARCHAR(128) NOT NULL,
+    measurements_id     INTEGER REFERENCES measurements(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS ingredientsrecipes (
-    id                  bigserial PRIMARY KEY,
-    ingredient_id       bigint REFERENCES ingredients(id) ON DELETE CASCADE,
-    recipe_id           bigint REFERENCES recipes(id) ON DELETE CASCADE,
+    id                  INTEGER PRIMARY KEY,
+    ingredient_id       INTEGER REFERENCES ingredients(id) ON DELETE CASCADE,
+    recipe_id           INTEGER REFERENCES recipes(id) ON DELETE CASCADE,
     amount              decimal(10,5) NOT NULL,
-    PRIMARY KEY (ingredient_id, recipe_id)
+    UNIQUE (ingredient_id, recipe_id)
 );
 
 CREATE TABLE IF NOT EXISTS measurements (
-    id                  bigserial PRIMARY KEY,
-    unit                varchar(38),
-    amount              int NOT NULL,
-    metric_unit         varchar(38)
+    id                  INTEGER PRIMARY KEY,
+    unit                VARCHAR(38),
+    amount              INTEGER NOT NULL,
+    metric_unit         VARCHAR(38)
+);
+
+CREATE TABLE IF NOT EXISTS history (
+    id                  INTEGER PRIMARY KEY,
+    user_id             INTEGER,
+    recipe_id           INTEGER REFERENCES recipes(id) ON DELETE CASCADE,
+    date                DATETIME,
+    choise              INTEGER
 );
 
 INSERT INTO 
-    measurements (id, unit, amount, metric_unit)
+    measurements (unit, amount, metric_unit)
 VALUES
-    (1,'ч.л.', 10, 'г'),
-    (2,'ст.л.', 25,'г'),
-    (3,'щепотка', 2, 'г'),
-    (4,'стакан', 200,'мл'),
-    (5,'по вкусу', 2,'г'),
-    (6,'на кончике ножа', 1,'г');
+    ('ч.л.', 10, 'г'),
+    ('ст.л.', 25,'г'),
+    ('щепотка', 2, 'г'),
+    ('стакан', 200,'мл'),
+    ('по вкусу', 2,'г'),
+    ('на кончике ножа', 1,'г'),
+    ('кг', 1000, 'г'),
+    ('л', 1000, 'мл');
     
